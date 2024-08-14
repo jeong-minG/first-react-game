aaad058ee638fa05d11724529f5ed98dbb4e0f80
jeong-minG <ika818488@gmail.com>
2024-08-14 21:59:18 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -5,36 +5,55 @@ import Box from './component/Box';
 const choice = {
   rock:{
     name:"Rock",
-    img:"https://i.ibb.co/fGpdQ6F/2.png"
+    img:"https://i.ibb.co/VVYNKZb/rock.png"
   },
   scissor:{
     name:"Scissor",
-    img:"https://i.ibb.co/KWNqzyq/3.png"
+    img:"https://i.ibb.co/K0xhny3/scissor.png"
   },
   paper:{
     name:"Paper",
-    img:"https://i.ibb.co/wWqcprD/4.png"
+    img:"https://i.ibb.co/mSpKcHJ/paper.png"
   }
 }
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
 
-  const [userSelect,setUserSelect] = useState(null)
-
-  const play = (userChoice) =>{
+  const play = (userChoice) => {
     // console.log("선택됨!",userChoice)
-    setUserSelect(choice[userChoice])
-  }
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) return "TIE";
+    else if (user.name === "Rock") return computer.name === "Scissor" ? "WIN" : "LOSE";
+    else if (user.name === "Scissor") return computer.name === "Paper" ? "WIN" : "LOSE";
+    else if (user.name === "Paper") return computer.name === "Rock" ? "WIN" : "LOSE";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 있는 키값만 뽑아서 배열로 만들어주는 함수
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+
   return (
     <div>
-    <div className='main'>
-      <Box title="You" item={userSelect}/> 
-      <Box title="Computer"/> 
-    </div>
-    <div className='main'>
-      <button onClick={()=>play("scissor")}>가위</button>
-      <button onClick={()=>play("rock")}>바위</button>
-      <button onClick={()=>play("paper")}>보</button>
-    </div>
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} /> 
+      <div className="buttons">
+        <button onClick={() => play("scissor")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+        <Box title="Computer" item={computerSelect}  result={result} />
+      </div>
     </div>
   );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Box = (props) => {
-  return (
-    <div className='box'>
-      <h1>{props.title}</h1>
-      <img src={props.item && props.item.img}/>
-      <h2>Win</h2>
-    </div>
-  )
-}
 
-export default Box
+    let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "TIE" &&
+    props.result !== ""
+  ) {
+    // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
+    result = props.result === "WIN" ? "LOSE" : "WIN";
+  } else {
+    // 위의 경우가 아니라면 props 로 전달된 결과를 그대로 쓴다.
+    result = props.result;
+  }
+  if (props.title === "Computer") {
+    console.log("computer", result);
+  }
+
+
+  return (
+    <div className={`box ${result}`}>
+      <h1>{props.title}</h1>
+      <img src={props.item && props.item.img} />
+      <h2>{result}</h2>
+    </div>
+  );
+};
+
+export default Box;
